77ddea4c96e7dabbf8c2c95cc4c94daa58b4c321
n01e0 <reoshiseki@gmail.com>
2021-01-22 16:27:51 +0900

    create options

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,5 @@
 extern crate clap;
 
 fn main() {
-    let app = clap_app!(pig =>
-        (name:          crate_name!())
-        (version:       crate_version!())
-        (author:        crate_authors!())
-        (about:         crate_description!())
-    ).get_matches();
+    let args = clap::App::from_yaml(load_yaml!("options.yml")).get_matches();
 }
